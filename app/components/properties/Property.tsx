5ad3b56cd8c4f6528217a24e3c4b7f9d1cd1f6ca
Sumit Choudhary <sumit.rc619@gmail.com>
2024-09-30 03:31:39 -0700

    first commit

---
+++ app/components/properties/Property.tsx
@@ -0,0 +1,13 @@
+import PropertyListItem from "./PropertyListItem"
+
+const Property = () => {
+  return (
+    <div>
+        <PropertyListItem/>
+        <PropertyListItem/>
+        <PropertyListItem/>
+    </div>
+  )
+}
+
+export default Property
