d71c75513e6607c006a5885d0f030111216f0a92
Sumit Choudhary <sumit.rc619@gmail.com>
2024-10-02 09:20:52 -0700

    Third

--- app/components/properties/PropertyListItem.tsx
+++ app/components/properties/PropertyListItem.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const PropertyListItem = () => {
   return (
-    <p>PropertyListItem</p>
-  )
-}
+    <div className="cursor-pointer">
+      <div className="relative overflow-hidden aspect-square rounded-xl">
+        <Image 
+          fill
+          src='/beachhouse.jpg'
+          alt=""
+          sizes="(max-width: 768px) 768px, (max-height: 1200px) 768px, 768px"
+          className="hover:scale-110 object-cover transition h-full w-full"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default PropertyListItem
+export default PropertyListItem;
