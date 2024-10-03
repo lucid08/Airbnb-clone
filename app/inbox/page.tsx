a8425d0ff47a4a4689a63e9dbb009122fb1e948f
Sumit Choudhary <sumit.rc619@gmail.com>
2024-10-03 17:05:23 +0530

    Conversation

---
+++ app/inbox/page.tsx
@@ -0,0 +1,9 @@
+const MyInboxPage = () => {
+    return (
+      <div className="max-w-[1500] mx-auto px-6 pb-6">
+        <h1 className="mb-6 text-2xl">MyInboxPage</h1>
+        </div>
+    )
+}
+
+export default MyInboxPage;
--- app/myreservations/page.tsx
+++ app/myreservations/page.tsx
@@ -17,20 +17,23 @@ const MyReservationPage = () => {
               />
             </div>
           </div>
-          <div className="col-span-1 md:col-span-3 space-y-2">
+          <div className="col-span-1 md:col-span-3 ">
             <h1 className="mb-4 text-xl">Properties</h1>
-            <p>
+            <p className="mb-2">
               <strong>Check in Date: </strong>22/22/22
             </p>
-            <p>
+            <p className="mb-2">
               <strong>Check out Date: </strong>22/22/22
             </p>
-            <p>
+            <p className="mb-2">
               <strong>No of Nights: </strong>5
             </p>
-            <p>
+            <p className="mb-2">
               <strong>Total price: </strong>840
             </p>
+            <div className="mt-6 cursor-pointer inline-block py-4 px-6 text-white bg-airbnb hover:bg-airbnb-dark rounded-xl">
+                Go to Properties....
+            </div>
           </div>
         </div>
         <div className="p-4 grid grid-cols-1 md:grid-cols-4 gap-4 rounded-xl border border-gray-300 shadow-xl">
@@ -44,20 +47,23 @@ const MyReservationPage = () => {
               />
             </div>
           </div>
-          <div className="col-span-1 md:col-span-3 space-y-2">
+          <div className="col-span-1 md:col-span-3 ">
             <h1 className="mb-4 text-xl">Properties</h1>
-            <p>
+            <p className="mb-2">
               <strong>Check in Date: </strong>22/22/22
             </p>
-            <p>
+            <p className="mb-2">
               <strong>Check out Date: </strong>22/22/22
             </p>
-            <p>
+            <p className="mb-2">
               <strong>No of Nights: </strong>5
             </p>
-            <p>
+            <p className="mb-2">
               <strong>Total price: </strong>840
             </p>
+            <div className="mt-6 cursor-pointer inline-block py-4 px-6 text-white bg-airbnb hover:bg-airbnb-dark rounded-xl">
+                Go to Properties....
+            </div>
           </div>
         </div>
       </div>
